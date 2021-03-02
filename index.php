20bb0c9f3b7ef057ec915b2cc9f125554b048266
Aer0m <veter1999@gmail.com>
2021-03-02 19:59:32 +0300

    thirdcommit

--- index.php
+++ index.php
@@ -16,49 +16,63 @@ $goods = [
 
     [
         "name" => "2014 Rossignol District Snowboard",
-        "category" => "boardsAndSki",
+        "category" => "Доски и лыжи",
         "price" => 10999,
         "URL_gif" => "img/lot-1.jpg"
     ],
 
     [
        "name" => "DC Ply Mens 2016/2017 Snowboard",
-        "category" => "boardsAndSki",
+        "category" => "Доски и лыжи",
         "price" => 159999,
         "URL_gif" => "img/lot-2.jpg"
     ],
 
     [
         "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
-        "category" => "fastenings",
+        "category" => "Крепления",
         "price" => 8000,
         "URL_gif" => "img/lot-3.jpg"
     ],
 
     [
         "name" => "Ботинки для сноуборда DC Mutiny Charocal",
-        "category" => "boots",
+        "category" => "Ботнки",
         "price" => 10999,
         "URL_gif" => "img/lot-4.jpg"
     ],
 
     [
         "name" => "Куртка для сноуборда DC Mutiny Charocal",
-        "category" => "clothes",
+        "category" => "Одежда",
         "price" => 7500,
         "URL_gif" => "img/lot-5.jpg"
     ],
 
     [
         "name" => "Маска Oakley Canopy",
-        "category" => "other",
+        "category" => "Разное",
         "price" => 5400,
         "URL_gif" => "img/lot-6.jpg"
-    ],
+    ]
 
 ];
 
+function sumFormat($value)
+{
 
+    $value = ceil($value);
+
+    if ($value < 1000)
+        return $value . '<b class=\"rub\">₽</b>';
+
+    elseif ($value >= 1000) {
+
+        number_format($value, 0, '.', ' ');
+        return $value . '<b class=\"rub\">₽</b>';
+    }
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -134,7 +148,7 @@ $goods = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost">цена</span>
+                                <span class="lot__cost"><?= sumFormat($lot['price']) ?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
