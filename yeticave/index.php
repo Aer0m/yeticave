809750e441acb01dbb057e831e684640c8383796
Aer0m <47530448+Aer0m@users.noreply.github.com>
2021-03-24 10:30:45 +0300

    lab10commit_unfinished

---
+++ yeticave/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once('functions.php');
+require_once('templates/data.php');
+
+$page_content = include_template('index.php',
+    ['categories' => $categories,
+        'goods' => $goods]);
+
+$layout_content = include_template('layout.php',
+    [
+        'title_name' => 'Главная',
+        'is_auth'=> $is_auth,
+        'user_name'=> $user_name,
+        'page_content' => $page_content,
+        'categories'=> $categories,
+        'user_avatar' => $user_avatar
+    ]);
+
+print($layout_content);
+
+?>
+
+
